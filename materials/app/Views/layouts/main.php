76fc7186150213a42374d5f02cefef2a898d8103
Jan Martinek <xmarti19@fi.muni.cz>
2022-10-17 01:28:57 +0200

    navbar expands again

--- materials/app/Views/layouts/main.php
+++ materials/app/Views/layouts/main.php
@@ -17,7 +17,7 @@
 
 <!-- bootstrap navigation bar --->
 <div class="container">
-    <nav class="navbar navbar-nav-fill-lg bg-white">
+    <nav class="navbar navbar-expand-lg bg-white">
         <div class="container-fluid">
             <a class="navbar-brand" href=<?= (isset($homeURL) ? $homeURL : '/')?>><img src="/assets/enai-logo_horizontal.png" width="auto" height="80px" alt="ENAI logo"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
