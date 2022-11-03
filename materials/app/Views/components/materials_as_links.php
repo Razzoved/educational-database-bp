15d961d5e941581a1425eda9784af07499fe0bf0
Jan Martinek <xmarti19@fi.muni.cz>
2022-11-03 19:42:01 +0100

    one post: image, title and goBack button done

--- materials/app/Views/components/materials_as_links.php
+++ materials/app/Views/components/materials_as_links.php
@@ -1,15 +1,16 @@
 <!-- group of MATERIALS shown as clickable links -->
 
-<ul>
+<ul class="list-unstyled">
     <?php foreach ($materials as $material) : ?>
     <li>
     <?php
-        // TODO: make it pretty
+        echo "<span class=\"badge bg-secondary\">$material->material_type</span>";
+        echo "  ";
         echo "<a href=\"$material->material_path\"";
         if ($material->material_type != 'link') {
             echo "download";
         }
-        echo "><span class=\"badge badge-secondary\">$material->material_type</span> $material->material_title</a>";
+        echo ">$material->material_title</a>";
     ?>
     </li>
     <?php endforeach; ?>
--- materials/app/Views/post_view_one.php
+++ materials/app/Views/post_view_one.php
@@ -1,25 +1,27 @@
 <?= $this->extend('layouts/main') ?>
 <?= $this->section('content') ?>
 
-
-<div class="parent-container d-flex"><!-- START OF PAGE SPLIT --->
+<!-- START OF PAGE SPLIT --->
+<div class="parent-container d-flex">
 
 <!-- Post container -->
-<div class="container" style="margin-bottom: 5vh;">
+<div class="container bg-light" style="height: 100vh">
 
     <!-- Post top view: img, header -->
-    <div class="row g-0 m-1">
+    <div class="row g-0 m-2">
 
         <!-- img -->
         <?= isset($post->referTo) ? "<a href='$post->referTo'>" : "" ?>
-        <img class="col-sm-12 col-md-4 img-fluid rounded float-left m-2" alt="thumbnail"
+        <img class="col-sm-12 col-md-4 img-fluid rounded float-left" alt="thumbnail"
              src=<?= $post->post_thumbnail ?>>
         <?= isset($post->referTo) ? "</a>" : "" ?>
 
         <!-- header: title, date, views, rating -->
-        <header class="col bg-light m-2" style="align-items: center; justify-content: center; margin-bottom: 1vh;">
-            <a href='/' class="btn btn-info">Back to main page</a>
-            <h1><?= $post->post_title ?></h1>
+        <header class="col p-3">
+            <div class="row g-0" style="align-items: center; justify-content: center">
+            <div class="col"><h1><?= $post->post_title ?></h1></div>
+            <div class="col-auto d-none d-md-inline"><a href='/' class="btn btn-dark">Go back</a></div>
+            </div>
         </header>
 
     </div>
@@ -29,29 +31,33 @@
     <hr>
 
     <!-- Content -->
-    <div>
+    <div class="m-2">
         <p><?= $post->post_content ?></p>
     </div>
 
     <hr>
 
     <!-- Materials -->
-    <div>
+    <div class="m-2">
         <?= view_cell('App\Libraries\Material::getMaterialsList', ['post' => $post]) ?>
     </div>
 
     <hr>
 
     <!-- Actions -->
-    <div>
+    <div class="m-2">
         <a href="/edit/<?= $post->post_id ?>" class="btn btn-primary">Edit</a>
         <a href="/delete/<?= $post->post_id ?>" class="btn btn-danger">Delete</a>
         <a href='/' class="btn btn-info">Back to main page</a>
     </div>
 </div>
 
+<!-- Padding -->
+<div class="flex-shrink-0 bg-white d-none d-lg-inline" style="height: 100vh; width: 10px;">
+</div>
+
 <!-- Tags sidebar shown ONLY on lg -->
-<div class="flex-shrink-0 p-3 bg-light d-none d-lg-inline" style="width: 280px;">
+<div class="flex-shrink-0 p-3 bg-light d-none d-lg-inline" style="height: 100vh; width: 280px;">
     <ul class="list-unstyled ps-0">
         <?php foreach ($post->getGroupedProperties() as $k => $v) : ?>
             <?= view_cell('App\Libraries\Property::postGroup', ['tag' => $k, 'values' => $v]) ?>
@@ -59,6 +65,7 @@
     </ul>
 </div>
 
-</div><!-- END OF PAGE SPLIT -->
+</div>
+<!-- END OF PAGE SPLIT -->
 
 <?= $this->endSection() ?>
