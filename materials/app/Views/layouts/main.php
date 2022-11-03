da3dd431dd63c433eefc204b4ab544d058e34950
Jan Martinek <xmarti19@fi.muni.cz>
2022-11-03 21:52:58 +0100

    one post: top done

--- materials/app/Views/layouts/main.php
+++ materials/app/Views/layouts/main.php
@@ -13,6 +13,9 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
+    <!-- Bootsrap icons CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
     <title><?= (isset($meta_title) ? $meta_title : 'Fallback title') ?></title>
 </head>
 
--- materials/app/Views/post_view_one.php
+++ materials/app/Views/post_view_one.php
@@ -5,10 +5,10 @@
 <div class="parent-container d-flex">
 
 <!-- Post container -->
-<div class="container bg-light" style="height: 100vh">
+<div class="container bg-light m-0" style="height: 100vh">
 
     <!-- Post top view: img, header -->
-    <div class="row g-0 m-2">
+    <div class="row g-0 mt-2">
 
         <!-- img -->
         <?= isset($post->referTo) ? "<a href='$post->referTo'>" : "" ?>
@@ -17,13 +17,33 @@
         <?= isset($post->referTo) ? "</a>" : "" ?>
 
         <!-- header: title, date, views, rating -->
-        <header class="col p-3">
-            <div class="row g-0" style="align-items: center; justify-content: center">
-            <div class="col"><h1><?= $post->post_title ?></h1></div>
-            <div class="col-auto d-none d-md-inline"><a href='/' class="btn btn-dark">Go back</a></div>
-            </div>
-        </header>
+        <header class="col p-3" style="align-items: center; justify-content: center">
 
+            <!-- title, goBack -->
+            <div class="row g-0">
+                <div class="col"><h1><?= $post->post_title ?></h1></div>
+                <div class="col-auto d-none d-md-inline"><a href='/' class="btn btn-dark">Go back</a></div>
+            </div>
+
+            <!-- date -->
+            <small ><p class="text-muted"><?= $post->post_created_at ?></p></small>
+
+            <div class="row-auto" style="align-items: center; justify-content: center">
+            </div>
+
+            <!-- rating, views -->
+            <div class="row g-1" style="align-items: center; justify-content: center">
+                <i class="col-auto bi bi-star-fill"></i>
+                <i class="col-auto bi bi-star-fill"></i>
+                <i class="col-auto bi bi-star-fill"></i>
+                <i class="col-auto bi bi-star"></i>
+                <i class="col-auto bi bi-star" style="margin-right: 0.2em"></i>
+                <small class="col-auto" style="margin-right: 0.5em"><?= $post->post_rating?></small>
+                <small class="col"><u><?= $post->post_rating?> ratings</u></small> <!-- TODO: implement this table and query -->
+                <small class="col-auto text-muted">Viewed: <?= $post->post_views ?>x</small>
+            </div>
+
+        </header>
     </div>
 
     <!-- Tags if screen too small -->
@@ -31,21 +51,21 @@
     <hr>
 
     <!-- Content -->
-    <div class="m-2">
+    <div>
         <p><?= $post->post_content ?></p>
     </div>
 
     <hr>
 
     <!-- Materials -->
-    <div class="m-2">
+    <div>
         <?= view_cell('App\Libraries\Material::getMaterialsList', ['post' => $post]) ?>
     </div>
 
     <hr>
 
     <!-- Actions -->
-    <div class="m-2">
+    <div>
         <a href="/edit/<?= $post->post_id ?>" class="btn btn-primary">Edit</a>
         <a href="/delete/<?= $post->post_id ?>" class="btn btn-danger">Delete</a>
         <a href='/' class="btn btn-info">Back to main page</a>
@@ -53,7 +73,7 @@
 </div>
 
 <!-- Padding -->
-<div class="flex-shrink-0 bg-white d-none d-lg-inline" style="height: 100vh; width: 10px;">
+<div class="bg-white d-none d-lg-inline" style="height: 100vh; width: 10px;">
 </div>
 
 <!-- Tags sidebar shown ONLY on lg -->
