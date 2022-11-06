b11168f450ae3fb873fd07075926dc69a7bc51f8
Jan Martinek <xmarti19@fi.muni.cz>
2022-11-07 00:04:10 +0100

    checkboxes reset update, TODO: jquery/javascript instead of post

--- materials/app/Views/widgets/offcanvas_checkboxes.php
+++ materials/app/Views/widgets/offcanvas_checkboxes.php
@@ -5,10 +5,12 @@
     <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasSidebar" aria-labelledby="offcanvasSidebarLabel">
         <div class="offcanvas-header">
             <h5 class="offcanvas-title" id="offcanvasSidebarLabel">Filters</h5>
-            <button class="btn btn-success">Search</button>
             <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
+            <button class="btn btn-success w-100 mb-2">Apply</button>
+            <!-- TODO: this should not do a post method, but a jquery? reset -->
+            <a class="btn btn-dark w-100 mb-2" href='/'>Reset all</a>
             <ul class="list-unstyled ps-0">
                 <?php foreach ($properties as $tag => $values) : ?>
                     <?= view('widgets/collapsible_list', ['tag' => $tag, 'values' => $values, 'type' => 'checkbox']) ?>
--- materials/app/Views/widgets/sidebar_checkboxes.php
+++ materials/app/Views/widgets/sidebar_checkboxes.php
@@ -2,6 +2,8 @@
      wrap it inside of a form with any action button -->
 <div class="flex-shrink-0 p-3 bg-light d-none d-lg-inline border" style="width: 280px">
     <ul class="list-unstyled ps-0 sticky" style="height: fit-content">
+        <!-- TODO: this should not do a post method, but a jquery? reset -->
+        <a class="btn btn-dark w-100 mb-2" href='/'>Reset all</a>
         <?php foreach ($properties as $tag => $values) : ?>
             <?= view('widgets/collapsible_list', ['tag' => $tag, 'values' => $values, 'type' => 'checkbox']) ?>
         <?php endforeach; ?>
