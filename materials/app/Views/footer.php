a121a9a10976a9c2ad9964909ccca740c5986da4
Jan Martinek <xmarti19@fi.muni.cz>
2023-03-01 20:41:10 +0100

    feat(footer): footer done

--- materials/app/Views/footer.php
+++ materials/app/Views/footer.php
@@ -1,24 +1,34 @@
-<footer class="footer bg-dark text-center text-white">
-    <div class="container p-2 pb-0">
+<footer>
+    <div class="container">
         <!-- Section: Social media -->
-        <section class="mb-2">
+        <section>
             <!-- Facebook -->
-            <a class="btn btn-outline-light btn-floating m-1" href="#" role="button">
+            <a href="https://www.facebook.com/academicintegrity.eu" role="button">
                 <i class="fa-brands fa-facebook"></i>
             </a>
 
             <!-- Twitter -->
-            <a class="btn btn-outline-light btn-floating m-1" href="#" role="button">
+            <a href="https://twitter.com/enaintegrity" role="button">
                 <i class="fa-brands fa-twitter"></i>
             </a>
 
+            <!-- LinkedIn -->
+            <a href="https://www.linkedin.com/in/enai-european-network-for-academic-integrity-9a4578168/" role="button">
+                <i class="fa-brands fa-linkedin"></i>
+            </a>
+
+            <!-- YouTube -->
+            <a href="https://www.youtube.com/@europeannetworkforacademic4752" role="button">
+                <i class="fa-brands fa-youtube"></i>
+            </a>
+
             <!-- Instagram -->
-            <a class="btn btn-outline-light btn-floating m-1" href="#" role="button">
+            <a href="https://www.instagram.com/enai_integrity/" role="button">
                 <i class="fa-brands fa-instagram"></i>
             </a>
 
             <!-- Mail -->
-            <a class="btn btn-outline-light btn-floating m-1" href="#" role="button">
+            <a href="mailto:%20info@academicintegrity.eu" role="button">
                 <i class="fa-solid fa-envelope"></i>
             </a>
         </section>
@@ -26,7 +36,6 @@
 
     <!-- Copyright -->
     <div class="copyrights" style="background-color: rgba(0, 0, 0, 0.2);">
-        © 2022 Copyright:
-        <a class="text-white" href="#">SOME LINK</a>
+        <p>© 2023 ENAI. All rights reserved</p>
     </div>
 </footer>
