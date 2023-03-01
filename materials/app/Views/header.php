3424b560d4816a91c13f8fea58d9065b377fc7c8
Jan Martinek <xmarti19@fi.muni.cz>
2023-03-01 17:12:13 +0100

    feat(css): removing bootstrap

--- materials/app/Views/header.php
+++ materials/app/Views/header.php
@@ -6,14 +6,7 @@
 <!-- Font Awesome kit CSS -->
 <script src="https://kit.fontawesome.com/b3b08eaee3.js" crossorigin="anonymous"></script>
 
-<!-- Bootstrap CSS -->
-<link rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
-        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
-        crossorigin="anonymous">
-
 <!-- Custom CSS -->
-<link rel="stylesheet" href="<?= base_url('/css/colors.css') ?>">
 <link rel="stylesheet" href="<?= base_url('/css/style.css') ?>">
 
 <!-- Tab title -->
