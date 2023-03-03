3d3e5d989fd0e6f266751bf4ae0fb34fc052e9fc
Jan Martinek <xmarti19@fi.muni.cz>
2023-03-03 02:48:04 +0100

    feat(views): should now work correctly

--- materials/app/Models/ViewsModel.php
+++ materials/app/Models/ViewsModel.php
@@ -5,6 +5,16 @@ namespace App\Models;
 use App\Entities\Material;
 use CodeIgniter\Model;
 
+/**
+ * This model handles the operations over the views table. The table
+ * stores the viewcount information for each material on daily basis
+ * (ie. increments the views for current day, or creates a new record).
+ *
+ * The model also provides a retrieval method for the latest most
+ * viewed materials.
+ *
+ * @author Jan Martinek
+ */
 class ViewsModel extends Model
 {
     protected $table = 'views';
@@ -20,9 +30,17 @@ class ViewsModel extends Model
     protected $createdField = 'created_at';
     protected $updatedField = '';
 
+    /**
+     * Grabs the 'n' most viewed materials and returns them as a numbered
+     * array. This array is already ordered by views. The view counts are
+     * also loaded into the material in place of the total views.
+     *
+     * @param int $n maximum number of materials to return
+     * @param int $days number of days to look back
+     */
     public function getTopMaterials(int $n, int $days = 30) : array
     {
-        $ids = $this->builder()
+        $views = $this->builder()
                     ->select('material_id')
                     ->selectSum('material_views')
                     ->groupBy('material_id')
@@ -32,7 +50,7 @@ class ViewsModel extends Model
                     ->getResultArray();
 
         $materials = array();
-        foreach ($ids as $index => $arr) {
+        foreach ($views as $index => $arr) {
             $material = model(MaterialModel::class)->getById((int) $arr['material_id']);
             $material->views = $arr['material_views'];
             $materials[] = $material;
@@ -42,10 +60,12 @@ class ViewsModel extends Model
     }
 
     /**
-     * Handles the update of views of given material id ONLY inside the
-     * views table.
+     * Handles the update of views of given material. Updates both this table
+     * and the material table.
+     *
+     * @param Material $material entity we want to increment views of
      */
-    public function increment(Material $material) : void
+    public function increment(Material &$material) : void
     {
         $all = $this->builder()
             ->select('*')
@@ -64,5 +84,32 @@ class ViewsModel extends Model
 
         $material->views++;
         model(MaterialModel::class)->update($material->id, $material);
+    }
+
+    /**
+     * This method imports all the viewcounts from the materials table
+     * to the views table, and sets their date to created_at from the
+     * material.
+     *
+     * @warning throws away any previous data (including dates).
+     * @depracated this is here to warn the developer
+     */
+    public function reimportMaterials()
+    {
+        $this->emptyTable($this->table);
+        $this->db->query('ALTER TABLE ' . $this->table . ' AUTO_INCREMENT = 1');
+
+        $lastKey = array_key_last($this->allowedFields);
+        $materials = model(MaterialModel::class)->getData(onlyPublic: false)->get()->getCustomResultObject(\App\Entities\Material::class);
+
+        $this->allowedFields[] = 'created_at';
+        foreach ($materials as $material) {
+            $this->insert([
+                'material_id' => $material->id,
+                'material_views' => $material->views,
+                'created_at' => $material->created_at->toDateString()
+            ]);
+        }
+        $this->allowedFields[$lastKey + 1] = null;
     }
 }
