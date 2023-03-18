661213c562917dabc6fd9783d82523541abcb048
Jan Martinek <xmarti19@fi.muni.cz>
2023-03-18 16:09:54 +0100

    refactor(views): navbars now conform to new style

--- materials/app/Views/admin/navigation_bar.php
+++ materials/app/Views/admin/navigation_bar.php
@@ -6,43 +6,61 @@
      */
     $refUrl = defined('ROOTURL') ? ROOTURL : base_url();
     $activePage = isset($activePage) ? $activePage : '';
+
+    $dash = $activePage === 'dashboard' ? ' active' : '';
+    $mats = $activePage === 'materials' ? ' active' : '';
+    $prop = $activePage === 'tags' ? ' active' : '';
+    $file = $activePage === 'files' ? ' active' : '';
+    $user = $activePage === 'users' ? ' active' : '';
 ?>
-<div class="navbar">
-    <nav class="container">
-        <img src="<?= base_url('public/assets/enai-logo-transparent.png') ?>"
-                alt="ENAI logo"
-                onclick="window.location.href='<?= $refUrl ?>'">
-        <a <?= $activePage === 'dashboard' ? 'class="active"' : '' ?> href="<?= base_url('') ?>")?>
-            <!-- <i class="fa-solid fa-gauge"></i> --> Public
-        </a>
-        <a <?= $activePage === 'materials' ? 'class="active"' : '' ?> href="<?= base_url('admin/materials') ?>")?>
-            <i class="fa-solid fa-box-archive"></i> Materials
-        </a>
-        <a <?= $activePage === 'tags' ? 'class="active"' : '' ?> href="<?= base_url('admin/tags') ?>")?>
-            <i class="fa-solid fa-tags"></i> Tags
-        </a>
-        <a <?= $activePage === 'files' ? 'class="active"' : '' ?> href="<?= base_url('admin/files') ?>")?>
-            <i class="fa-solid fa-file"></i> Files
-        </a>
-        <a <?= $activePage === 'users' ? 'class="active"' : '' ?> href="<?= base_url('admin/users') ?>")?>
-            <i class="fa-solid fa-user"></i> Users
-        </a>
-        <a class="login" href="<?= base_url('/admin/logout') ?>">
-            Logout <i class="fa-sharp fa-solid fa-right-from-bracket"></i>
-        </a>
-        <a class="icon" href="javascript:void(0);" onclick="navbar_toggle()">
-            <i class="fa fa-bars"></i>
-        </a>
-    </nav>
-</div>
+<nav class="navbar">
+    <div class="navbar__container">
+        <img class="navbar__log"
+            src="<?= base_url('public/assets/enai-logo-transparent.png') ?>"
+            alt="ENAI logo"
+            onclick="window.location.href='<?= $refUrl ?>'">
+        <ul class="navbar__list">
+            <li class="navbar__item">
+                <a class="navbar__button<?= $dash ?>" href="<?= base_url('') ?>")?>
+                    <!-- <i class="fa-solid fa-gauge"></i> --> Public
+                </a>
+            </li>
+            <li class="navbar__item">
+                <a class="navbar__button<?= $mats ?>" href="<?= base_url('admin/materials') ?>")?>
+                    <i class="fa-solid fa-box-archive"></i> Materials
+                </a>
+            </li>
+            <li class="navbar__item">
+                <a class="navbar__button<?= $prop ?>" href="<?= base_url('admin/tags') ?>")?>
+                    <i class="fa-solid fa-tags"></i> Tags
+                </a>
+            </li>
+            <li class="navbar__item">
+                <a class="navbar__button<?= $file ?>" href="<?= base_url('admin/files') ?>")?>
+                    <i class="fa-solid fa-file"></i> Files
+                </a>
+            </li>
+            <li class="navbar__item">
+                <a class="navbar__button<?= $user ?>" href="<?= base_url('admin/users') ?>")?>
+                    <i class="fa-solid fa-user"></i> Users
+                </a>
+            </li>
+            <li class="navbar__item navbar__item--to-right">
+                <a class="navbar__button" href="<?= base_url('/admin/logout') ?>">
+                    <i class="fa-sharp fa-solid fa-right-from-bracket"></i> Logout
+                </a>
+            </li>
+            <li class="navbar__item">
+                <a class="navbar__toggle" href="javascript:void(0);" onclick="navbar_toggle()">
+                    <i class="fa fa-bars"></i>
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
 
 <script type="text/javascript">
     function navbar_toggle() {
-        var x = document.querySelector(".navbar");
-        if (x.className === "navbar") {
-            x.className += " responsive";
-        } else {
-            x.className = "navbar";
-        }
+        document.querySelector(".navbar").classList.toggle('navbar--responsive');
     }
 </script>
--- materials/app/Views/navigation_bar.php
+++ materials/app/Views/navigation_bar.php
@@ -6,25 +6,50 @@
      * @var string $activePage last url segment of the current page
      */
     $refUrl = defined('ROOTURL') ? ROOTURL : base_url();
+
     $activePage = isset($activePage) ? $activePage : '';
+    $default = $activePage === '' ? ' active' : '';
+    $topRated = $activePage === 'top-rated' ? ' active' : '';
+    $mostViewed = $activePage === 'most-viewed' ? ' active' : '';
 ?>
-<div class="navbar">
-    <nav class="container">
-        <img src="<?= base_url('public/assets/enai-logo-transparent.png') ?>"
-                alt="ENAI logo"
-                onclick="window.location.href='<?= $refUrl ?>'">
-        <a <?= $activePage === '' ? 'class="active"' : '' ?> href="<?= base_url() ?>">All materials</a>
-        <a <?= $activePage === 'top-rated' ? 'class="active"' : '' ?> href="<?= base_url('top-rated') ?>">Top rated</a>
-        <a <?= $activePage === 'most-viewed' ? 'class="active"' : '' ?> href="<?= base_url('most-viewed') ?>">Most viewed</a>
-        <a class="login" href="<?= base_url('admin') ?>"><?= session('isLoggedIn') ? 'To administration' : 'Login' ?></a>
-        <a class="icon" href="javascript:void(0);" onclick="navbar_toggle()">
-            <i class="fa fa-bars"></i>
-        </a>
-    </nav>
-</div>
+<nav class="navbar">
+    <div class="navbar__container">
+        <img class="navbar__logo"
+            src="<?= base_url('public/assets/enai-logo-transparent.png') ?>"
+            alt="ENAI logo"
+            onclick="window.location.href='<?= $refUrl ?>'">
+        <ul class="navbar__list">
+            <li class="navbar__item<?= $default ?>">
+                <a class="navbar__button" href="<?= base_url() ?>">
+                    All materials
+                </a>
+            </li>
+            <li class="navbar__item<?= $topRated ?>">
+                <a class="navbar__button" href="<?= base_url('top-rated') ?>">
+                    Top rated
+                </a>
+            </li>
+            <li class="navbar__item<?= $mostViewed ?>">
+                <a class="navbar__button" href="<?= base_url('most-viewed') ?>">
+                    Most viewed
+                </a>
+            </li>
+            <li class="navbar__item navbar__item--to-right">
+                <a class="navbar__button" href="<?= base_url('admin') ?>">
+                    <?= session('isLoggedIn') ? 'To administration' : 'Login' ?>
+                </a>
+            </li>
+            <li class="navbar__item">
+                <a class="navbar__toggle" href="javascript:void(0);" onclick="navbar_toggle()">
+                    <i class="fa fa-bars"></i>
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
 
 <script type="text/javascript">
     function navbar_toggle() {
-        document.querySelector(".navbar").classList.toggle('responsive');
+        document.querySelector(".navbar").classList.toggle('navbar--responsive');
     }
 </script>
