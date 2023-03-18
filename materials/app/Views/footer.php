275be81898bd45bffe979ffb911de55e273c3675
Jan Martinek <xmarti19@fi.muni.cz>
2023-03-18 11:32:07 +0100

    feat(style): footer now adheres to BEM

--- materials/app/Views/footer.php
+++ materials/app/Views/footer.php
@@ -1,41 +1,39 @@
-<footer>
-    <div class="container">
-        <!-- Section: Social media -->
-        <section>
-            <!-- Facebook -->
-            <a href="https://www.facebook.com/academicintegrity.eu" role="button">
-                <i class="fa-brands fa-facebook"></i>
-            </a>
+<footer class="footer">
+    <!-- Section: Social media -->
+    <section class="footer__big-area">
+        <!-- Facebook -->
+        <a href="https://www.facebook.com/academicintegrity.eu" role="button">
+            <i class="footer__icon fa-brands fa-facebook"></i>
+        </a>
 
-            <!-- Twitter -->
-            <a href="https://twitter.com/enaintegrity" role="button">
-                <i class="fa-brands fa-twitter"></i>
-            </a>
+        <!-- Twitter -->
+        <a href="https://twitter.com/enaintegrity" role="button">
+            <i class="footer__icon fa-brands fa-twitter"></i>
+        </a>
 
-            <!-- LinkedIn -->
-            <a href="https://www.linkedin.com/in/enai-european-network-for-academic-integrity-9a4578168/" role="button">
-                <i class="fa-brands fa-linkedin"></i>
-            </a>
+        <!-- LinkedIn -->
+        <a href="https://www.linkedin.com/in/enai-european-network-for-academic-integrity-9a4578168/" role="button">
+            <i class="footer__icon fa-brands fa-linkedin"></i>
+        </a>
 
-            <!-- YouTube -->
-            <a href="https://www.youtube.com/@europeannetworkforacademic4752" role="button">
-                <i class="fa-brands fa-youtube"></i>
-            </a>
+        <!-- YouTube -->
+        <a href="https://www.youtube.com/@europeannetworkforacademic4752" role="button">
+            <i class="footer__icon fa-brands fa-youtube"></i>
+        </a>
 
-            <!-- Instagram -->
-            <a href="https://www.instagram.com/enai_integrity/" role="button">
-                <i class="fa-brands fa-instagram"></i>
-            </a>
+        <!-- Instagram -->
+        <a href="https://www.instagram.com/enai_integrity/" role="button">
+            <i class="footer__icon fa-brands fa-instagram"></i>
+        </a>
 
-            <!-- Mail -->
-            <a href="mailto:%20info@academicintegrity.eu" role="button">
-                <i class="fa-solid fa-envelope"></i>
-            </a>
-        </section>
-    </div>
+        <!-- Mail -->
+        <a href="mailto:%20info@academicintegrity.eu" role="button">
+            <i class="footer__icon fa-solid fa-envelope"></i>
+        </a>
+    </section>
 
     <!-- Copyright -->
-    <div style="background-color: rgba(0, 0, 0, 0.2);">
-        <p>© 2023 ENAI. All rights reserved</p>
-    </div>
+    <section class="footer__small-area">
+        <p class="footer__text">© 2023 ENAI. All rights reserved</p>
+    </section>
 </footer>
