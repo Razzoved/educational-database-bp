ce065aee44ae7ae77806875b1d95aca9deac701e
Jan Martinek <xmarti19@fi.muni.cz>
2023-03-21 23:07:13 +0100

    fix(style): property buttons now have a cleaner look

--- materials/app/Views/components/property_button.php
+++ materials/app/Views/components/property_button.php
@@ -5,9 +5,9 @@
      */
 ?>
 
-<form method="post" action='<?= base_url('/1') ?>' style="display: block">
-    <input type="submit"
-            name="<?= esc($tag) ?>[<?= esc($value) ?>]" value="<?= esc($value) ?>"
-            style="white-space:normal; word-wrap: normal"
-    >
+<form method="get" action='<?= base_url('/1') ?>' style="display: block">
+    <input type="hidden"
+            name="<?= esc($tag) ?>[<?= esc($value) ?>]"
+            value="on">
+    <button type="submit"><?= esc($value) ?></button>
 </form>
