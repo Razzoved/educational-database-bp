a995f5de3e1f5decc9120cdbec6b2813ac4529b6
Jan Martinek <xmarti19@fi.muni.cz>
2023-04-26 20:22:03 +0200

    feat(footer): added about link, may be further changed later

--- materials/app/Views/footer.php
+++ materials/app/Views/footer.php
@@ -2,33 +2,39 @@
     <!-- Section: Social media -->
     <section class="footer__big-area">
         <!-- Facebook -->
-        <a href="https://www.facebook.com/academicintegrity.eu" role="button">
+        <a target="_blank" href="https://www.facebook.com/academicintegrity.eu" role="button">
             <i class="footer__icon fa-brands fa-facebook"></i>
         </a>
 
         <!-- Twitter -->
-        <a href="https://twitter.com/enaintegrity" role="button">
+        <a target="_blank" href="https://twitter.com/enaintegrity" role="button">
             <i class="footer__icon fa-brands fa-twitter"></i>
         </a>
 
         <!-- LinkedIn -->
-        <a href="https://www.linkedin.com/in/enai-european-network-for-academic-integrity-9a4578168/" role="button">
+        <a target="_blank" href="https://www.linkedin.com/in/enai-european-network-for-academic-integrity-9a4578168/" role="button">
             <i class="footer__icon fa-brands fa-linkedin"></i>
         </a>
 
         <!-- YouTube -->
-        <a href="https://www.youtube.com/@europeannetworkforacademic4752" role="button">
+        <a target="_blank" href="https://www.youtube.com/@europeannetworkforacademic4752" role="button">
             <i class="footer__icon fa-brands fa-youtube"></i>
         </a>
 
         <!-- Instagram -->
-        <a href="https://www.instagram.com/enai_integrity/" role="button">
+        <a target="_blank" href="https://www.instagram.com/enai_integrity/" role="button">
             <i class="footer__icon fa-brands fa-instagram"></i>
         </a>
 
         <!-- Mail -->
-        <a href="mailto:%20info@academicintegrity.eu" role="button">
+        <a target="_blank" href="mailto:%20info@academicintegrity.eu" role="button">
             <i class="footer__icon fa-solid fa-envelope"></i>
+        </a>
+    </section>
+
+    <section class="footer__small-area">
+        <a href="http://www.academicintegrity.eu/wp/about-enai" style="color: white">
+            About
         </a>
     </section>
 
