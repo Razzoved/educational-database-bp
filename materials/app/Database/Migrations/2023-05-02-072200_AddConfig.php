7f893a793114d1a96aa536c2a21b2d4b1e29be5f
Jan Martinek <xmarti19@fi.muni.cz>
2023-05-02 08:32:51 +0200

    feat(migration): added config migration

---
+++ materials/app/Database/Migrations/2023-05-02-072200_AddConfig.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+use Exception;
+
+/**
+ * Creates a new table in the database. This table is meant to contain
+ * configuration information settable by administrators.
+ *
+ * (example: default material image, about page location, etc.)
+ *
+ * @author Jan Martinek
+ */
+class AddConfig extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'config_id' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 64,
+            ],
+            'config_value' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 2048,
+            ],
+        ]);
+        $this->forge->addPrimaryKey('config_id');
+        $this->forge->createTable('config');
+
+        try {
+            $defaults = [
+                [
+                    'config_id' => 'default_image',
+                    'config_value' => 'assets/default_image.png'
+                ],
+                [
+                    'config_id' => 'about_page',
+                    'config_value' => 'https://www.academicintegrity.eu/wp/about-enai'
+                ],
+                [
+                    'config_id' => 'home_page',
+                    'config_value' => 'https://www.academicintegrity.eu'
+                ],
+            ];
+            $this->db->table('config')->insertBatch($defaults);
+        } catch (Exception $e) {
+            $this->forge->dropTable('config');
+            throw $e;
+        }
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('config');
+    }
+}
