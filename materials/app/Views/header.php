cd711fa8c5eab63adc46e56f21660f43365b2e42
Jan Martinek <xmarti19@fi.muni.cz>
2023-05-05 23:42:11 +0200

    feat(header): now loads params from _GET first

--- materials/app/Views/header.php
+++ materials/app/Views/header.php
@@ -12,6 +12,6 @@
 <!-- last search data, used for dynamic page elements -->
 <script>
     const params = new URL(window.location).searchParams;
-    if (!params.has('sort')) params.set('sort', 'id');
-    if (!params.has('sortDir')) params.set('sortDir', 'desc');
+    if (!params.has('sort')) params.set('sort', '<?= $_GET['sort'] ?? 'id' ?>');
+    if (!params.has('sortDir')) params.set('sortDir', '<?= $_GET['sortDir'] ?? 'desc' ?>');
 </script>
