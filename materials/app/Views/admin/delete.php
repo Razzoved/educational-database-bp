6b308a7156e0f336f12c716020ab262712853337
Jan Martinek <xmarti19@fi.muni.cz>
2023-05-05 23:41:49 +0200

    feat(delete): delete modal updated

--- materials/app/Views/admin/delete.php
+++ materials/app/Views/admin/delete.php
@@ -5,7 +5,7 @@
      * @param string $itemType type of item to be deleted (material/...)
      */
 ?>
-<form class="modal" id="delete-window">
+<div class="modal" id="modal-delete">
 
     <div class="modal__content">
         <div class="modal__header">
@@ -14,66 +14,60 @@
         </div>
 
         <div class="modal__body">
-            <p id="delete-message">
-                Warning: This action is irreversible!<br>
-                Are you sure you want to delete <?= $itemType ?? 'item' ?> with id: <strong>[]</strong>?
-            </p>
+            <form class="form" data-method="DELETE">
+                <p id="delete-message">
+                    Warning: This action is irreversible!<br>
+                    Are you sure you want to delete <?= $itemType ?? 'item' ?> with id: <strong>[]</strong>?
+                </p>
+            </form>
         </div>
 
         <div class="modal__footer">
             <div class="modal__button-group">
-                <button type="submit" class="modal__button modal__button--red">Delete</button>
+                <button type="submit" class="modal__button modal__button--red" onclick="deleteSubmit()">Delete</button>
                 <button type="button" class="modal__button" onclick="deleteClose()">Cancel</button>
             </div>
         </div>
-</form>
+    </div>
 
     <script type="text/javascript">
-        const deleteModal = document.getElementById("delete-window");
+        <?php include_once(FCPATH . 'js/modal.js'); ?>
 
-        const titleElement = deleteModal.querySelector('#delete-title');
-        const titleOriginal = titleElement.innerHTML;
+        const deleteModal = document.getElementById("modal-delete");
+        const deleteAction = "<?= $action ?>";
+        const deleteSubmitBtn = deleteModal.querySelector("button[type='submit']");
 
         const messageElement = deleteModal.querySelector('#delete-message');
         const messageOriginal = messageElement.innerHTML;
 
+        const deleteOpenAll = (action) => {
+            if (action === undefined || action === null) {
+                console.error("Action not defined");
+                return;
+            }
+            messageElement.innerHTML = messageOriginal.replace('[]', `[ALL]`);
+            deleteModal.querySelector("form").action = action;
+            deleteModal.classList.add('modal--visible');
+        }
+
         const deleteOpen = (id) => {
+            if (id === undefined || id === null) {
+                console.error("Invalid id", id);
+                return;
+            }
             messageElement.innerHTML = messageOriginal.replace('[]', `[${id}]`);
-            deleteModal.setAttribute('data-value', id);
+            deleteModal.querySelector("form").action = deleteAction.replace(/@segment@/, id);
             deleteModal.classList.add('modal--visible');
         }
 
         const deleteClose = () => {
-            deleteModal.classList.remove('modal--visible');
+            modalClose(null, 'modal-delete');
         }
 
-        window.addEventListener("click", function(event)
-        {
-            if (event.target == deleteModal) {
-                deleteClose();
-            }
-        });
+        const deleteSubmit = () => {
+            modalSubmit(true, 'modal-delete');
+        }
 
-        deleteModal.addEventListener("submit", async function(event) {
-            event.preventDefault();
-
-            const response = await fetch(
-                '<?= $action ?>'.replace(/([0-9]+|@segment@)$/, deleteModal.getAttribute('data-value')),
-                { method: 'DELETE' }
-            );
-
-            if (!response.ok) {
-                messageElement.innerHTML ="Item with id: <strong>" +
-                    deleteModal.getAttribute('data-value') +
-                    "</strong> could not be deleted." +
-                    "<br><em style='margin: 1rem; color: red;'>" + response.statusText + "</em>" +
-                    "<br>Do you want to try again?";
-                return;
-            }
-
-            const data = await response.json();
-            document.getElementById(data.id)?.remove();
-            deleteClose();
-        });
+        window.addEventListener("click", (event) => event.target === deleteModal && deleteClose());
     </script>
-</div>
+</form>
