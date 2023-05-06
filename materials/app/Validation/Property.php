603542b45e9ea7bd02dc3fa4a4a6a5d9f258722e
Jan Martinek <xmarti19@fi.muni.cz>
2023-05-06 16:25:04 +0200

    fix(property): update validation should now work correctly

--- materials/app/Validation/Property.php
+++ materials/app/Validation/Property.php
@@ -4,13 +4,43 @@ namespace App\Validation;
 
 class Property
 {
-    public function property_value_update(string $value, int $tag, int $id) : bool
+    public function property_value_update(string $value, string $params, array $data, ?string &$error = null) : bool
     {
+        if (!isset($value)) {
+            $error = 'Missing value!';
+            return false;
+        }
+
+        $params = explode(',', $params);
+        if (sizeof($params) != 2) {
+            $error = 'Invalid parameters for property update, should be - tag, id!';
+            return false;
+        }
+
+        $tag = $params[0];
+        $id = $params[1];
+
+        if (!isset($data[$tag])) {
+            $error = 'Missing id of category!';
+            return false;
+        }
+
+        if (!is_numeric($data[$tag])) {
+            $error = strtoupper($tag) . ' must be a number!';
+            return false;
+        }
+
         $properties = model(PropertyModel::class)
-            ->where('property_tag', $tag)
+            ->where('property_tag', (int) $data[$tag])
             ->where('property_value', $value)
             ->findAll(2);
-        $count = array_count_values($properties);
-        return $count === 0 || ($count === 1 && isset($id) && $properties[0]->id === $id);
+        $count = sizeof($properties);
+
+        return $count === 0 || (
+            isset($data[$id]) &&
+            is_numeric($data[$id]) &&
+            $count === 1 &&
+            $properties[0]->id === (int) $data[$id]
+        );
     }
 }
