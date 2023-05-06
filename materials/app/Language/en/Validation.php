ab788aa893714cc3aa6542b447750f51504df3ba
Jan Martinek <xmarti19@fi.muni.cz>
2023-05-06 23:50:42 +0200

    fix(validation): another typo

--- materials/app/Language/en/Validation.php
+++ materials/app/Language/en/Validation.php
@@ -5,8 +5,8 @@ return [
     'property_value_update' => 'Category already contains a tag with this VALUE!',
     'user_name_update'      => 'User with this NAME already exists!',
     'user_email_update'     => 'User with this EMAIL already exists!',
-    'user_email'            => 'User with this email adress does not exist!',
+    'user_email'            => 'User with this EMAIL adress does not exist!',
     'user_password'         => 'Invalid password!',
-    'valid_status'          => 'Invalid status, should be one of: <pre>' . print_r(\App\Entities\Cast\StatusCast::VALID_VALUES, true) . '</pre>',
-    'valid_tag'             => 'Circular dependency deteceted at TAG!',
+    'valid_status'          => 'Invalid status!',
+    'valid_tag'             => 'Circular dependency detected at TAG!',
 ];
