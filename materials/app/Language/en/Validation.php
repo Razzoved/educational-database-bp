ca04e126fc03aa58b4bb1ccc8c7b04c0f9fde5da
Jan Martinek <xmarti19@fi.muni.cz>
2023-05-06 16:24:40 +0200

    feat(validation): new messages

--- materials/app/Language/en/Validation.php
+++ materials/app/Language/en/Validation.php
@@ -1,4 +1,11 @@
 <?php
 
 // override core en language system validation or define your own en language validation message
-return [];
+return [
+    'property_value_update' => 'Category already contains a tag with this VALUE!',
+    'user_name_update'      => 'User with this NAME already exists!',
+    'user_email_update'     => 'User with this EMAIL already exists!',
+    'user_email'            => 'User with this email adress does not exist!',
+    'user_password'         => 'Invalid password!',
+    'valid_status'          => 'Invalid status, should be one of: <pre>' . print_r(\App\Entities\Cast\StatusCast::VALID_VALUES, true) . '</pre>',
+];
