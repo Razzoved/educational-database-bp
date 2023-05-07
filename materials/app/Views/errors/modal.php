fcd027c7e10f30a472ff08a0b5ad4a6502d84f6f
Jan Martinek <xmarti19@fi.muni.cz>
2023-05-07 21:56:32 +0200

    feat(error): modal fixed to current version

--- materials/app/Views/errors/modal.php
+++ materials/app/Views/errors/modal.php
@@ -2,25 +2,31 @@
     /** Intended primarily for use by controllers.
      *
      * @param string $title
-     * @param string $message 
+     * @param string $message
      */
+    $title = $title ?? "@title@";
+    $message = $message ?? "@message@";
 ?>
 
-<div class="modal" tabindex="-1" role="dialog" id="error-modal">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
+<div class="modal modal--visible" tabindex="-1" id="error-modal">
 
-            <div class="modal-header">
-                <h1 id="error-title" class="modal-title"><?= $title ?></h1>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="document.getElementById('error-modal').remove()">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
+    <div class="modal__content">
 
-            <div class="modal-body">
-                <p id="error-message"><?= $message ?><p>
-            </div>
-
+        <div class="modal__header">
+            <h1 class="modal__title" id="error-title"><?= $title ?></h1>
+            <span class="modal__close" onclick="document.getElementById('error-modal').remove()">&#10005;</span>
         </div>
+
+        <div class="modal__body">
+            <p id="error-message" style="color: red">Error: <?= $message ?><p>
+        </div>
+
+        <div class="modal__footer">
+            <div class="modal__button-group">
+                <button type="button" class="modal__button" onclick="document.getElementById('error-modal').remove()">Close</button>
+            </div>
+        </div>
+
     </div>
+
 </div>
